af0b8b95bffbf9538f097332a54c30d134bc401a
wildlife7 <wildlifesantury@gmail.com>
2017-04-11 00:50:53 +0530

    First Commit

---
+++ wildlife/js/index.js
@@ -0,0 +1,91 @@
+
+var $item = $('.carousel .item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
+
+
+
+var lock = new Auth0Lock('3uZdfqoJlsxfOzsURijaga9hErS8AqYS', 'wildlifesantury.auth0.com');
+
+
+// app.js
+
+var btn_login = document.getElementById('btn-login');
+
+btn_login.addEventListener('click', function() {
+  lock.show();
+});
+
+
+
+
+
+// app.js
+
+lock.on("authenticated", function(authResult) {
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+      // Handle error
+      return;
+    }
+    localStorage.setItem('id_token', authResult.idToken);
+    // Display user information
+    show_profile_info(profile);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
